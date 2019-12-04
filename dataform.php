8ac94254ce52ae41704bf4e8be1dfb42807f0eaf
PinCesa <58314876+PinCesa@users.noreply.github.com>
2019-12-04 20:21:38 -0300

    v

--- dataform.php
+++ dataform.php
@@ -8,6 +8,6 @@ $descripcion = $_POST['descripcion'];
 $image = $_POST['imagen'];
 $categoria_id = $_POST['categoria'];
 $user_id= 1;
-// echo $titulo." ".$descripcion." ".$image." ".$categoria_id.". ".$user_id;ikmkg
+// echo $titulo." ".$descripcion." ".$image." ".$categoria_id.". ".$user_id;
 
-createPost($titulo,$descripcion,$image,$user_id,$categoria_id);
+newPost($titulo,$descripcion,$image,$user_id,$categoria_id);
